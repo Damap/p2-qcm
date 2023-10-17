01731a46d1df4da7d5eb31826101bb1bd7ea0ac6
Luca4Z <luca.stevens@icloud.com>
2023-10-17 17:59:37 +0200

    ajout du time

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import qcm
 from difficulty import quoting, set_difficulty
 import random
+import time
 
 if __name__ == '__main__':
-    filename = "./QCM.txt"
+    filename = 'p2-qcm/QCM.txt'
     print("Bienvenue sur le QCM de projet 2 !")
     """
     user = (input("Choissisez votre nom d'utilisateur C> "))
@@ -28,8 +29,10 @@ if __name__ == '__main__':
     random.shuffle(questions)
     for q in range(len(questions)):
         print("\tQ| " + str(q + 1) + ": \"" + questions[q][0] + "\"")
+        time.sleep(3)
         for r in range(len(questions[q][1])):
             print("\t\tA|" + str(r + 1) + ":" + f"\t{questions[q][1][r][0]}""")
+            time.sleep(0.5)
         while True:
             try:
                 answer = int(input(f"{user} C> "))
@@ -37,11 +40,16 @@ if __name__ == '__main__':
                 continue
             if 1 <= answer <= len(questions[q][1]):
                 if questions[q][1][answer-1][1]:
+                    time.sleep(2)
                     print("Bonne réponse !")
+                    time.sleep(2)
                 else:
+                    time.sleep(2)
                     print("Mauvaise réponse")
+                    time.sleep(2)
                 if questions[q][1][answer - 1][2] != "":
                     print("Feedback: \"" + questions[q][1][answer - 1][2] + "\"")
+                    time.sleep(2)
                 quotes.append(quoting(questions[q][1][answer-1][1], q+1, diff)) #Renvoi true ou false
                 break
             else:
@@ -51,6 +59,8 @@ if __name__ == '__main__':
                     elif diff == 2:
                         quotes.append(-1)
                     break
+                time.sleep(1)
                 print("Cette réponse n'est pas disponible, si vous ne voulez pas répondre tapez -1")
+                time.sleep(1)
                 continue
     print(f"Notes total : {quotes}")
