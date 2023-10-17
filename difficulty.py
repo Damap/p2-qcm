d197f8399a26f8b74e9af0f31ce67737fb6d9290
Damap <damien.pelaez@student.uclouvain.be>
2023-10-17 15:57:57 +0200

    Ajustement et ajout de fonctionnalité

--- difficulty.py
+++ difficulty.py
@@ -1,23 +1,34 @@
 def set_difficulty(difficulty):
     """Trois niveau de difficulté :
         Sympa / Sévère / Mystère"""
-    opts = ["sympa", "severe", "mystere"]
-
-
-    diff = 0
     if difficulty == "sympa":
-        diff = 1
+        return 1
     elif difficulty == "severe":
-        diff = 2
-    else:
-        diff = 3
-    return diff
-
+        return 2
+    else: #difficulty random
+        return 3
 
 """
 Le param Q est le paramètre qui permet de savoir à quel questions on se trouve
 Le param D est le niveau de difficulté choisi avant (1, 2 ou 3)
+
+il faut changer quelque peu ce code, pour utilisé des 0, 1 et -1 en tant que valeur de réponse plus booléen
+pour gérer les cas d'abstinence de réponse
 """
 def quoting(answer, q, d):
-    return False
+    if d == 1:
+        if answer:
+            return 1
+        else:
+            return 0
+
+    elif d == 2:
+        if answer:
+            return 1
+        else:
+            return -1
+    else:
+        #à coder
+
+        return True
 
--- main.py
+++ main.py
@@ -1,42 +1,45 @@
 import qcm
-from difficulty import quoting
+from difficulty import quoting, set_difficulty
 
 if __name__ == '__main__':
     filename = "./QCM.txt"
     print("Bienvenue sur le QCM de projet 2 !")
-    """
     user = (input("Choissisez votre nom d'utilisateur C> "))
     print(f"Bonjour {user}")
     difficulty_choice = ""
     while difficulty_choice == "" or difficulty_choice not in ["sympa", "severe", "mystere"]:
         print("Vous devez choisir un niveau existant !")
         difficulty_choice = input("Sélectionné un niveau de difficulté (sympa/severe/mystere) C> ")
-    """
-    #temp
-    user = "Damien"
+    diff = set_difficulty(difficulty_choice)
+
     print("\n\n") #passement de ligne
     # Chargement du questionnaire
     questions = qcm.build_questionnaire(filename)
     print("Lecture du questionnaire.")
+    quotes = []
+    #modifié aléatoirement l'ordre des questions
     for q in range(len(questions)):
-        print("\tQuestion " + str(q + 1) + ": \"" + questions[q][0] + "\"")
-        print("\tPropositions de réponses")
+        print("\tQ| " + str(q + 1) + ": \"" + questions[q][0] + "\"")
         for r in range(len(questions[q][1])):
-            print("\t\tReponses " + str(r + 1) + ":")
-            print("\t\t\tMessage: \"" + questions[q][1][r][0] + "\"")
+            print("\t\tA|" + str(r + 1) + ":" + f"\t{questions[q][1][r][0]}""")
         while True:
             answer = int(input(f"{user} C> "))
             if 1 <= answer <= len(questions[q][1]):
-                quoting(questions[q][1][answer-1][1], 1, 1) #Renvoi true ou false
                 if questions[q][1][answer-1][1]:
                     print("Bonne réponse !")
-                    if questions[q][1][answer-1][2] != "":
-                        print("\t\t\tFeedback: \"" + questions[q][1][answer-1][2] + "\"")
                 else:
                     print("Mauvaise réponse")
-                    if questions[q][1][answer-1][2] != "":
-                        print("Feedback: \"" + questions[q][1][answer-1][2] + "\"")
+                if questions[q][1][answer - 1][2] != "":
+                    print("Feedback: \"" + questions[q][1][answer - 1][2] + "\"")
+                quotes.append(quoting(questions[q][1][answer-1][1], q+1, diff)) #Renvoi true ou false
                 break
             else:
+                if answer == -1:
+                    if diff == 1:
+                        quotes.append(0)
+                    elif diff == 2:
+                        quotes.append(-1)
+                    break
+                print("Cette réponse n'est pas disponible, si vous ne voulez pas répondre tapez -1")
                 continue
-
+    print(f"Notes total : {quotes}")
